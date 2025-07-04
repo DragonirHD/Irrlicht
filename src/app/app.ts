ac5e28ebc88e376b8d13461dcd05fb707a0331fb
DragonirHD <dragonirhd@gmail.com>
2025-07-05 00:10:37 +0200

    app: - moved sidenavModeToggleButton into its own component - sidenav now closes when changing the route if it is in "over"-mode (should feel better on handheld devices)
    sidenav-mode-toggle:
    - added localStorage support for saving selected sidenav mode
    - added initial setting of sidenav mode on first site visit according to window.width

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,34 @@
 import {Component, ViewChild} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatButtonModule, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
-import {MatDrawer, MatDrawerMode, MatSidenavModule} from '@angular/material/sidenav';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
 import {ThemeToggleComponent} from '../components/theme-toggle/theme-toggle';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatListModule} from '@angular/material/list';
 import {AnimationHandler} from '../services/animation-handler';
 import {AnimationType} from '../common/classes/AnimationType';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {SidenavModeToggle} from '../components/sidenav-mode-toggle/sidenav-mode-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatIcon, MatSidenavModule, ThemeToggleComponent, MatButtonModule, MatMenuModule, RouterLink, MatListModule, MatTooltipModule],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatSidenavModule, ThemeToggleComponent, MatButtonModule, MatMenuModule, RouterLink, MatListModule, MatTooltipModule, SidenavModeToggle],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   @ViewChild("sidenav") sidenav: MatDrawer | undefined;
   @ViewChild("sidenavToggleButton") sidenavToggleButton: MatIconButton | undefined;
-  @ViewChild("sidenavModeToggleButton") sidenavModeToggleButton: MatIconButton | undefined;
 
-  protected sidenavMode: MatDrawerMode = "over";
-  constructor(private readonly animationHandler: AnimationHandler) {
+  constructor(private readonly animationHandler: AnimationHandler, private router: Router) {
+    //on routing changes, the sidenav should close if it is in the "over" mode.
+    this.router.events.subscribe(() => {
+      if (this.sidenav?.mode == "over") {
+        this.sidenav.close();
+      }
+    })
   }
 
   protected onSidenavToggleButtonClick(): void {
@@ -34,23 +39,6 @@ export class App {
       });
     }
     this.sidenav?.toggle();
-  }
-
-  protected onSidenavModeToggleButtonClick(): void {
-    if (this.sidenav?.mode == "over") {
-      this.sidenavMode = "side";
-    } else {
-      this.sidenavMode = "over";
-    }
-
-    if (this.sidenavModeToggleButton) {
-      console.log(this.sidenavMode)
-      this.animationHandler.playOnce({
-        nativeElement: this.sidenavModeToggleButton?._elementRef.nativeElement,
-        animationType: AnimationType.RotationHalf,
-        playReverse: this.sidenavMode == "side"
-      });
-    }
   }
 
   protected onSiteNamePointerEnter(): void {
