16a8e6679495c16b0f0bccdc81f6964efb83d3c6
DragonirHD <dragonirhd@gmail.com>
2025-08-31 19:32:54 +0200

    [projects-site] projects: - reworked behaviour and styling of mat-tab-group to work more like a carousel - added simple swiping support for mobile
    styles:
    - added mat-tab overrides
    - increased border size of gradient-border

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal, ViewChild, WritableSignal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {MatTab, MatTabGroup, MatTabLabel} from '@angular/material/tabs';
 import {ProjectSummaryBase} from './project-summary-base/project-summary-base';
@@ -19,7 +19,11 @@ import {CardSelector} from '../../components/card-selector/card-selector';
   styleUrl: './projects.scss'
 })
 export class Projects implements OnInit {
+  @ViewChild('summaryTabGroup') summaryTabGroup: MatTabGroup | undefined;
   protected projects: Project[] = [];
+  protected summaryTabGroupSelectedIndex: number = 0;
+  protected lastTouchStart: WritableSignal<Touch | undefined> = signal(undefined);
+  protected lastTouchMove: WritableSignal<Touch | undefined> = signal(undefined);
 
   constructor(private route: ActivatedRoute, private projectService: ProjectService) {
   }
@@ -32,5 +36,32 @@ export class Projects implements OnInit {
 
     this.projects = await this.projectService.getProjects();
     console.log(this.projects);
+  }
+
+  /**Used to add very limited swipe support to our tab group.*/
+  protected summarySwipeEnd() {
+    //We need to use the value of "touchmove" here instead of "touchend" because "touchend.touches" will only have items if we use multiple pointers.
+
+    //if we have a no value for the start or end position of our swipe, we can't calculate where we swiped.
+    if (!this.lastTouchStart() || !this.lastTouchMove()) {
+      return;
+    }
+
+    if (this.lastTouchStart()!.clientX > this.lastTouchMove()!.clientX
+      && this.summaryTabGroupSelectedIndex < this.summaryTabGroup!._tabs.length) {
+      this.summaryTabGroupSelectedIndex++;
+    }
+
+    if (this.lastTouchStart()!.clientX < this.lastTouchMove()!.clientX
+      && this.summaryTabGroupSelectedIndex > 0) {
+      this.summaryTabGroupSelectedIndex--;
+    }
+  }
+
+  protected summarySwipeCancel() {
+    //if we run into a situation where the swipe is cancelled,
+    //we reset the last saved values to not accidentally swipe if the user didn't decide to do so.
+    this.lastTouchStart.set(undefined);
+    this.lastTouchMove.set(undefined);
   }
 }
